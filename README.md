98e6f5d4ba7cafcd145195b7663790d1b03feff6
Anton Maminov <anton.linux@gmail.com>
2024-04-05 11:18:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# ipapi-crystal
+
+Crystal bindings for <https://ipapi.co> (IP Address Location & Geolocation API)
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     ipapi:
+       github: geocrystal/ipapi-crystal
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "ipapi"
+
+location = client.locate("50.1.2.3")
+# => #<Ipapi::Location:0x7b45cdce5b40 @ip="50.1.2.3", @network="50.1.0.0/21", @version="IPv4", @city="Antelope", @region="California", @region_code="CA", @country="US", @country_name="United States", @country_code="US", @country_code_iso3="USA", @country_capital="Washington", @country_tld=".us", @continent_code="NA", @in_eu=false, @postal="95843", @latitude=38.7169, @longitude=-121.3677, @timezone="America/Los_Angeles", @utc_offset="-0700", @country_calling_code="+1", @currency="USD", @currency_name="Dollar", @languages="en-US,es-US
+,haw,fr", @country_area=9629091.0, @country_population=327167434, @asn="AS7065", @org="SNIC">
+
+location.ip                   # "50.1.2.3"
+location.network              # "50.1.0.0/21"
+location.version              # "IPv4"
+location.city                 # "Antelope"
+location.region               # "California"
+location.region_code          # "CA"
+location.country              # "US"
+location.country_name         # "United States"
+location.country_code         # "US"
+location.country_code_iso3    # "USA"
+location.country_capital      # "Washington"
+location.country_tld          # ".us"
+location.continent_code       # "NA"
+location.in_eu                # false
+location.postal               # "95843"
+location.latitude             # 38.7169
+location.longitude            # -121.3677
+timezone                      # "America/Los_Angeles"
+location.utc_offset           # "-0700"
+location.country_calling_code # "+1"
+location.currency             # "USD"
+location.currency_name        # "Dollar"
+location.languages            # "en-US,es-US,haw,fr"
+location.country_area         # 9629091.0
+location.country_population   # 327167434
+location.asn                  # "AS7065"
+location.org                  # "SNIC"
+```
+
+## Contributing
+
+1. Fork it (<https://github.com/geocrystal/ipapi-crystal/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Anton Maminov](https://github.com/mamantoha) - creator and maintainer
---
+++ src/ipapi.cr
@@ -0,0 +1,119 @@
+require "http/client"
+require "json"
+
+# Crystal bindings for https://ipapi.co (IP Address Location & Geolocation API)
+#
+# API Docs : https://ipapi.co/api/
+module Ipapi
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
+  class Client
+    API_URL = "https://ipapi.co/"
+
+    def initialize(@api_key : String? = nil)
+    end
+
+    # Location of a specific IP
+    def locate(ip_address : String) : Location?
+      url = "#{API_URL}#{ip_address}/json"
+      url = url + "?key=#{@api_key}" if @api_key
+
+      response = HTTP::Client.get(url)
+
+      parse_locate_response(response)
+    end
+
+    # Location of client’s IP
+    def locate : Location?
+      url = "#{API_URL}json"
+      url = url + "?key=#{@api_key}" if @api_key
+
+      response = HTTP::Client.get(url)
+
+      parse_locate_response(response)
+    end
+
+    private def parse_locate_response(response : HTTP::Client::Response) : Location?
+      case response.status_code
+      when 200
+        if JSON.parse(response.body).as_h["error"]? == true
+          error_response = ErrorResponse.from_json(response.body)
+
+          raise Error.new(error_response.reason)
+        end
+
+        Location.from_json(response.body)
+      when 403
+        raise AuthorizationFailedException.new
+      when 404
+        raise PageNotFoundException.new
+      when 429
+        raise RateLimitedException.new
+      else
+        raise Error.new(response.body)
+      end
+    end
+  end
+
+  # Generic error.
+  class Error < Exception
+  end
+
+  class AuthorizationFailedException < Error
+    def initialize
+      super "Invalid authorization credentials : HTTP 403"
+    end
+  end
+
+  class PageNotFoundException < Error
+    def initialize
+      super "Request was rate limited : HTTP 429"
+    end
+  end
+
+  class RateLimitedException < Error
+    def initialize
+      super "Request was rate limited : HTTP 429"
+    end
+  end
+
+  class Location
+    include JSON::Serializable
+
+    property ip : String
+    property network : String
+    property version : String
+    property city : String
+    property region : String
+    property region_code : String
+    property country : String
+    property country_name : String
+    property country_code : String
+    property country_code_iso3 : String
+    property country_capital : String
+    property country_tld : String
+    property continent_code : String
+    property in_eu : Bool
+    property postal : String
+    property latitude : Float64
+    property longitude : Float64
+    property timezone : String
+    property utc_offset : String
+    property country_calling_code : String
+    property currency : String
+    property currency_name : String
+    property languages : String
+    property country_area : Float64
+    property country_population : Int32
+    property asn : String
+    property org : String
+  end
+
+  class ErrorResponse
+    include JSON::Serializable
+
+    property ip : String
+    property error : Bool
+    property reason : String
+  end
+end
